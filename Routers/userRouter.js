a288494e5124c6ba9a405f5c31370ff4fabf5326
YadhuZoan <yadhuy28@gmail.com>
2023-09-27 14:02:33 +0530

    otp about half complete

--- Routers/userRouter.js
+++ Routers/userRouter.js
@@ -5,7 +5,23 @@ router.get('/',(req,res)=>{
     res.render('user/userLogin')
 })
 router.get('/signup',(req,res)=>{
-    res.render('user/user-signup.ejs')
+    res.render('user/userSignUp')
+})
+router.get('/otpsend',(req,res)=>{
+    res.render('user/otpRegister')
+})
+router.get('/userHome',(req,res)=>{
+
+})
+
+
+
+
+router.post('/otp',(req,res)=>{
+    res.redirect('/otpsend')
+})
+router.post('/home',(req,res)=>{
+    res.redirect('/userHome')
 })
 
 module.exports = router
