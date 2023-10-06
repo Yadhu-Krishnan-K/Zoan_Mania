18f39eae036861836d62005aefb4c011e1e638de
YadhuZoan <yadhuy28@gmail.com>
2023-10-06 14:56:29 +0530

    otp worked , project 1st week 1/4 completed

--- Routers/supAdminRoute.js
+++ Routers/supAdminRoute.js
@@ -1,14 +1,32 @@
 const express = require('express')
 const router = express.Router()
+const db = require('../models/user')
+
+const credential = {
+    email : "a@s.m",
+    password : "123"
+}
 
 router.get('/',(req,res)=>{
     res.render('supAdmin/admin-login')
 })
-router.get('/adminUserControl',(req,res,next)=>{
-    res.render('supAdmin/sidebar')
+router.get('',)
+router.get('/adminUserControl',async(req,res,next)=>{
+    let i = 0;
+    const userData = await db.find();
+    res.render('supAdmin/admin-control-user',{title:"userdata",userData,i})
     // res.send('oky')
 })
 
+//inventory
+router.get('/admin/inventory',(req,res)=>{
+    res.render('supAdmin/admin-inventory')
+})
+
+//users
+router.get('/admin/Customers',(req,res)=>{
+    res.render('supAdmin/adnim-control-user')
+})
 
 
 
@@ -17,7 +35,11 @@ router.get('/adminUserControl',(req,res,next)=>{
 router.post('/adminDash',(req,res,next)=>{
     // console.log(req.body);
     // res.send('okx')
-    res.redirect('/admin/adminUserControl')
+    if(req.body.email===credential.email && req.body.password==credential.password){
+        res.redirect('/admin/adminUserControl')
+    }else{
+        res.send('something went wrong')
+    }
 })
 
 
---
+++ controllers/for-otp.js
@@ -0,0 +1,76 @@
+require('dotenv').config()
+const nodemailer = require('nodemailer')
+const Mailgen = require('mailgen')
+const monmodel= require('../models/user')
+
+
+//otp generator
+const otpGenerator = require('otp-generator')
+
+var vaotp=otpGenerator.generate(4, { digits: true, specialChars: false, lowerCaseAlphabets: false, upperCaseAlphabets: false })
+
+// console.log(vaotp)
+// sent mail with real email account
+ const otp = async(req,res)=>{
+
+    
+  const {name,email,password}=req.body;
+  const logged=await monmodel.create({name,email,password});
+  if (logged) {
+      let config = {
+        service : 'gmail',
+        auth : {
+          user:process.env.EMAIL,
+          pass:process.env.PASSWORD
+        }
+      }
+      
+      let transporter = nodemailer.createTransport(config)
+
+      let MailGenerator =new Mailgen({
+        theme:"default",
+        product:{
+          name:"Mailgen",
+          link:'https://mailgen.js/'
+        }
+      })
+      let response = {
+        body:{
+          name:name,
+          intro:`YOUR OTP FOR ZOAN MANiA ${vaotp}`,
+          outro:"Looking forward to do more business"
+        }
+
+      }
+      let mail =MailGenerator.generate(response)
+      let message = {
+        from:process.env.email,
+        to:email,
+        subject:'OTP VARIFICATION',
+        html:mail
+      }
+
+      
+      transporter.sendMail(message).then(()=>{
+        return res.status(201).json({
+          msg:"you should receive an email"
+        })
+      }).catch(error => {
+        return res.status(500)
+      })
+
+    res.redirect('/otpsen')
+  
+
+  }else{
+      console.log("something went wrong");
+  }
+
+
+}
+
+
+    // res.status(201).json("getBill Successfully....!")
+
+
+module.exports = {otp,vaotp}
