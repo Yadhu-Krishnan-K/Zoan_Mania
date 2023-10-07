d54fbc9273ee58a48d024dbcb21f11ecaf93af8c
YadhuZoan <yadhuy28@gmail.com>
2023-10-07 17:38:04 +0530

    front home page almost complete

---
+++ controllers/home-page-products.js
@@ -0,0 +1,24 @@
+const objects = [
+    {
+        pic:"/product_pics/picc1.png",
+        heading:"Jumper With Decorative Detailing",
+        offer:"UP TO 38% OFF"
+    },
+    {
+        pic:"/product_pics/picc1.png",
+        heading:"Jumper With Decorative Detailing",
+        offer:"UP TO 38% OFF"
+    },
+    {
+        pic:"/product_pics/picc1.png",
+        heading:"Jumper With Decorative Detailing",
+        offer:"UP TO 38% OFF"
+    },
+    {
+        pic:"/product_pics/picc1.png",
+        heading:"Jumper With Decorative Detailing",
+        offer:"UP TO 38% OFF"
+    }
+]
+
+module.exports = objects
