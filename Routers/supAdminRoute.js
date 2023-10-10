42278b3b3da93ff063d6886a3259ede9f539c9a8
YadhuZoan <yadhuy28@gmail.com>
2023-10-10 21:54:34 +0530

    user signin/signup almost done

--- Routers/supAdminRoute.js
+++ Routers/supAdminRoute.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const db = require('../models/user')
+const produ = require('../models/products')
+const Cate = require('../models/category')
 
 const credential = {
     email : "a@s.m",
@@ -10,37 +12,79 @@ const credential = {
 router.get('/',(req,res)=>{
     res.render('supAdmin/admin-login')
 })
-router.get('',)
-router.get('/adminUserControl',async(req,res,next)=>{
+// router.get('',)
+//---------------------------------------------------------------------------------4
+//customer
+
+router.get('/Customers',async(req,res,next)=>{
     let i = 0;
     const userData = await db.find();
-    res.render('supAdmin/admin-control-user',{title:"userdata",userData,i})
+    res.render('supAdmin/admin-control-user',{userData,i})
     // res.send('oky')
 })
 
+// -----------------------------------------------------------------------------------------------------//
 //inventory
-router.get('/admin/inventory',(req,res)=>{
+router.get('/inventory',(req,res)=>{
     res.render('supAdmin/admin-inventory')
+    // res.send('hello')
 })
+router.get('/inventory/addProduct',(req,res)=>{
+    res.render('supAdmin/admin-addProduct')
+    // res.send('hai')
+})
+
 
 //users
-router.get('/admin/Customers',(req,res)=>{
-    res.render('supAdmin/adnim-control-user')
-})
+// router.get('/admin/Customers',(req,res)=>{
+//     res.redirect('/admin/adminUserControl')
+// })
 
 
-
+//-----------------------------------------------------------------------------------------------------------//
 
 
 router.post('/adminDash',(req,res,next)=>{
     // console.log(req.body);
     // res.send('okx')
     if(req.body.email===credential.email && req.body.password==credential.password){
-        res.redirect('/admin/adminUserControl')
+        res.redirect('/admin/Customers')
     }else{
         res.send('something went wrong')
     }
+}) 
+
+// ------------------------------------------------------------------------------------------------------------//
+
+//insert product
+router.get('/inventory/addProduct',(req,res)=>{
+    res.render('supAdmin/admin-addProduct')
 })
+
+
+
+
+//''''''''''''''''''''''''''''''''''''''''''''''''''''------------------------------------------------------------------------//
+
+
+
+
+
+//logout
+
+router.get('/logout',(req,res)=>{
+    res.redirect('/admin');
+})
+
+
+
+
+
+
+
+
+
+
 
 
 module.exports = router
--- Routers/userRouter.js
+++ Routers/userRouter.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const us = require('../controllers/user-side')
 const userModel = require('../models/user')
+// const monmodel= require('../models/user')
+
 const productInHome = require('../controllers/home-page-products')
 
 
@@ -13,34 +15,67 @@ router.get('/signup',(req,res)=>{
     res.render('user/userSignUp')  
 })
 
-router.get('/otpsen',(req,res)=>{
-    res.render('user/otpRegister')
-})
+// router.get('/otpsen',(req,res)=>{
+//     // console.log(controller.vaotp)
+//     // const {name,email,password}=req.session.data;
+//     // const logged=await userModel.create({name,email,password});
+//     // console.log(logged)
+//     res.render('user/otpRegister')
+// })
+
+
+//--------------------------------------------------------------------------------------------------------------------------//
+//userHome
+
 
 router.get('/userHome',(req,res)=>{
-    res.render('user/userHome',{productInHome})
+    const name = req.session.name
+    console.log(name)
+    res.render('user/userHome',{productInHome,name})
 })
 
 
 
 
 router.post('/otpsend',controller.otp);
- 
-router.post('/home',(req,res)=>{
-    let userOtp=req.body.number1
-    // console.log(controller.vaotp)
-    console.log(req.body.number1)
-    if(userOtp==controller.vaotp){
 
+router.get('/otpsen',(req,res)=>{
+    console.log(controller.vaotp)
+    res.render('user/otpRegister')
+})
+ 
+router.post('/home',async(req,res)=>{
+    let userOtp=req.body.number1
+    console.log(controller.vaotp)
+    const {name,email,password}=req.session.data;
+    req.session.name = name
+    console.log(req.body.number1)
+    console.log(controller.vaotp)
+    const logins = await userModel.findOne({email:email})
+if(!logins){
+    if(userOtp==controller.vaotp){
+        
+        const logged=await userModel.create({name,email,password});
+        console.log(logged)
      res.redirect('/userHome')
     }else{
         res.send('something went wrong')
+        // res.render('/signup',{text:"enter valid otp"})
     }
+}else{
+    // res.render('/signup',{text:"email already exist"})
+    res.send('email already exists')
+}
 })
+
+
+
 router.post('/homed',async(req,res)=>{
     const {email,password} = req.body;
     
     const logins = await userModel.findOne({email:email,password:password})
+    console.log(logins)
+    req.session.name = logins.name
     if(logins){
         res.redirect('/userHome')
     }else{
