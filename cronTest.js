24554806046f5f4a5a5fe6b529ed3093366220bf
YadhuZoan <yadhuy28@gmail.com>
2024-01-22 23:38:27 +0530

    deleted some unwanted files

--- cronTest.js
+++
@@ -1,7 +0,0 @@
-const cron = require('node-cron')
-// cron.schedule('1,2,4,5 * * * *', () => {
-//     console.log('running every minute 1, 2, 4 and 5');
-//   });
-cron.schedule('*/10 * * * * *', () => {
-    console.log('running a task every 10 secondary');
-  });
